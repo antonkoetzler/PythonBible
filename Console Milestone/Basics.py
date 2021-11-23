d2c7cb4b839650098562a62c7b16207dac2db34a
Anton Koetzler-Faust <anton.koetzlerfaust@uleth.ca>
2021-11-22 23:24:56 -0700

    Added a file

---
+++ Console Milestone/Basics.py
@@ -0,0 +1,20 @@
+# Very basics to python
+# This is how you make a comment
+
+a = 5 # Creating a variable
+b = a + 5
+
+print("Hello World")
+
+print("\nHere are some variables I made:")
+print("   a: ", a)
+print("   b: ", b)
+
+print("\n")
+for x in range(5):
+    print("This is a basic for loop in python, iteration: ", x)
+
+print("\n")
+while a != 0:
+    print("This is a basic while loop in python, iteration: ", a)
+    a -= 1
---
+++ Console Milestone/README.md
@@ -0,0 +1,5 @@
+## Basics.py
+1. Variables
+2. Printing
+3. Loops
+4. Commenting
