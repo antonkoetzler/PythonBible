003623a3c8d64a1c41e23ba653cefca2eb7e808e
Anton Koetzler-Faust <anton.koetzlerfaust@uleth.ca>
2021-12-07 10:21:10 -0700

    Added a sample for input and output

---
+++ PIP Packages/README.md
@@ -0,0 +1,8 @@
+What is PIP?
+- Python's package manager
+- It's acronyms
+  - Python Installs PIP (recurive acronym)
+  - Preferred Installer Program
+- Popular packages from pip
+  - Requests
+  - youtube-dl
---
+++ Terminal Milestone/IO.py
@@ -0,0 +1,10 @@
+inp1 = input("Enter value, always returned as a string: ")
+print("inp1: ", inp1, " ~ inp1's datatype: ", type(inp1))
+
+print()
+inp2 = int(input("Enter an integer value, if it's not, the program crashes: "))
+print("inp2: ", inp2, " ~ inp2's datatype: ", type(inp2))
+
+print()
+inp3 = float(input("Now I'm threatening you to enter a floating point number to continue: "))
+print("inp3: ", inp3, " ~ inp3's datatype: ", type(inp3))
